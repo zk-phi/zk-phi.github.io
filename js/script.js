dae662b51c2525486671d1e1b8e2cc2b790908af
zk-phi <zk-phi@users.noreply.github.com>
2020-09-14 21:28:21 +0900

    Tweak footer style

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ getUpdatedDatetime(function (datetime) {
 });
 
 window.onscroll = function () {
-    if (window.scrollY > 256) {
+    if (window.scrollY > 128) {
         document.getElementsByTagName("footer")[0].classList.add("hidden");
     }
 };
