ececbac19aff6b8fc09ba28276ec507517584f51
zk-phi <zk-phi@users.noreply.github.com>
2021-02-10 20:24:28 +0900

    Run js after load

--- js/script.js
+++ js/script.js
@@ -1,24 +1,17 @@
-/* Get the time string of this branch's last commit. */
-function getUpdatedDatetime (handler, errHandler) {
+window.onload = function () {
+    // apply 'hover' to touched elements in mobile devices
+    document.addEventListener('touchstart', function () {}, { passive: true });
+    // get last-updated datetime
     var xhr = new XMLHttpRequest();
     xhr.responseType = "json";
     xhr.onload = function () {
         try {
             var jstDate = (new Date(this.response.commit.commit.author.date)).toLocaleString();
-            handler(jstDate);
+            document.getElementById("lastUpdated").innerHTML = jstDate;
         } catch (e) {
-            errHandler && errHandler(e);
+            document.getElementById("lastUpdated").innerHTML = "N/A";
         }
     };
     xhr.open("GET", "https://api.github.com/repos/zk-phi/zk-phi.github.io/branches/master");
     xhr.send();
-}
-
-getUpdatedDatetime(function (datetime) {
-    document.getElementById("lastUpdated").innerHTML = datetime;
-}, function (e) {
-    document.getElementById("lastUpdated").innerHTML = "N/A";
-});
-
-// apply 'hover' to touched elements in mobile devices
-document.addEventListener('touchstart', function () {}, { passive: true });
+};
