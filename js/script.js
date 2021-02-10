1be03635482ccacafd4ec49babceaedde0eb6585
zk-phi <zk-phi@users.noreply.github.com>
2021-02-10 12:16:11 +0900

    Use passive listener

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 function getUpdatedDatetime (handler, errHandler) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = "json";
-    xhr.onload = function () {
+    xhr.addEventListener('load', function () {
         try {
             var jstDate = (new Date(this.response.commit.commit.author.date)).toLocaleString();
             handler(jstDate);
         } catch (e) {
             errHandler && errHandler(e);
         }
-    };
+    }, { passive: true });
     xhr.open("GET", "https://api.github.com/repos/zk-phi/zk-phi.github.io/branches/master");
     xhr.send();
 }
