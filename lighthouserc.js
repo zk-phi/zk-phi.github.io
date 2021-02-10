ee52ac1738b9a33c4c7127c503da7d6baffdac6a
zk-phi <zk-phi@users.noreply.github.com>
2021-02-10 20:15:01 +0900

    Remove some assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,6 +7,10 @@ module.exports = {
         },
         assert: {
             preset: "lighthouse:no-pwa",
+            assertions: {
+                "uses-long-cache-ttl": "off",
+                "render-blocking-resources": ["warn", { maxNumericValue: 1 }],
+            },
         },
         upload: {
             target: "temporary-public-storage",
