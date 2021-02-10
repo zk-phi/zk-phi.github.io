7384ac9a4d041f51149bbc567d52fd90676e1cc3
zk-phi <zk-phi@users.noreply.github.com>
2021-02-10 20:12:27 +0900

    Fix format issues

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,16 +1,15 @@
 module.exports = {
-  ci: {
-    collect: {
-      numberOfRuns: 3,
-      staticDistDir: "./",
-      url: ['index.html'],
-    },
-    "assert": {
-      preset: "lighthouse:no-pwa",
-    },
-    "upload": {
-      "target": "temporary-public-storage",
-    },
-  }
+    ci: {
+        collect: {
+            numberOfRuns: 3,
+            staticDistDir: "./",
+            url: ['index.html'],
+        },
+        assert: {
+            preset: "lighthouse:no-pwa",
+        },
+        upload: {
+            target: "temporary-public-storage",
+        },
+    }
 };
-
