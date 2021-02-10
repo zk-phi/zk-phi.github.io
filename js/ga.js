5c0c4e3048cd52ccd98a4c7f5159648cc8d64b79
zk-phi <zk-phi@users.noreply.github.com>
2021-02-10 14:58:44 +0900

    Try analytics.js

--- js/ga.js
+++ js/ga.js
@@ -1,13 +1,15 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-gtag('config', 'UA-144855098-1');
+ga('create', 'UA-XXXXX-Y', 'auto');
+ga('send', 'pageview');
 
 function clap () {
     var btn = document.getElementById("clap");
     btn.className += " disabled";
     btn.href = "javascript: void 0;";
-    gtag('event', 'clap');
+    ga('send', 'event', 'general', 'clap');
     window.alert("応援ありがとうございます！");
 }
