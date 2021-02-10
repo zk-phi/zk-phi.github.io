83b51cf1fc9a672df7c29d3e926d5798ad605052
zk-phi <zk-phi@users.noreply.github.com>
2021-02-10 12:23:53 +0900

    Use passive listener for touch events

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 function getUpdatedDatetime (handler, errHandler) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = "json";
-    xhr.addEventListener('load', function () {
+    xhr.onload = function () {
         try {
             var jstDate = (new Date(this.response.commit.commit.author.date)).toLocaleString();
             handler(jstDate);
         } catch (e) {
             errHandler && errHandler(e);
         }
-    }, { passive: true });
+    };
     xhr.open("GET", "https://api.github.com/repos/zk-phi/zk-phi.github.io/branches/master");
     xhr.send();
 }
@@ -27,3 +27,6 @@ function handleScroll () {
     }
 }
 document.addEventListener('scroll', handleScroll, { passive: true });
+
+// apply 'hover' to touched elements in mobile devices
+document.addEventListener('touchstart', function () {}, { passive: true });
