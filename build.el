f1b84337ec303deb7717b275a2455b873eb8f979
zk-phi <zk-phi@users.noreply.github.com>
2021-02-11 15:40:36 +0900

    Optimize etc.html

--- build.el
+++ build.el
@@ -4,13 +4,13 @@
 
 (setq default-directory (or (getenv "GIT_ROOT") default-directory))
 
-(dolist (file '("index.html" "index_en.html"))
+(dolist (file '("index.html" "index_en.html" "etc.html"))
   (with-temp-buffer
     (insert-file-contents file)
     ;; embed lastUpdated date
     (save-excursion
-      (search-forward-regexp "<span id=\"lastUpdated\">\\([^<]*\\)</span>")
-      (replace-match timestamp t t nil 1))
+      (when (search-forward-regexp "<span id=\"lastUpdated\">\\([^<]*\\)</span>" nil t)
+        (replace-match timestamp t t nil 1)))
     ;; inline core CSSs
     (save-excursion
       (while (search-forward-regexp
