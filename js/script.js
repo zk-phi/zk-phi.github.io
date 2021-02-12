152d8d8ced41d6f64ddec4cae85ef0cd937873c6
zk-phi <zk-phi@users.noreply.github.com>
2021-02-12 18:58:24 +0000

    deploy: bd195886968b0e196a3816829a2f340641fcf843

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function clap () {
-    var btn = document.getElementById("clap");
-    btn.className += " disabled";
-    btn.href = "javascript: void 0;";
-    window.alert("応援ありがとうございます！");
-}
-
-document.addEventListener('touchstart', function () {}, { passive: true });
