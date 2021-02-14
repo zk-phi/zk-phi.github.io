3f62ed8af8575edc3252b1f7430cfa26ca714ce4
zk-phi <zk-phi@users.noreply.github.com>
2021-02-14 17:52:53 +0900

    Make link take className as an arg

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -2,16 +2,17 @@ import * as React from "react";
 import { Link as GatsbyLink } from "gatsby";
 import style from "../styles/Link.module.css";
 
-const Link = ({ href, to, children }) => {
+const Link = ({ className, href, to, children }) => {
+    const name = `${className} ${style.link}`;
     if (href) {
         return (
-            <a target="_blank" rel="noreferrer" href={ href } className={ style.link }>
+            <a target="_blank" rel="noreferrer" href={ href } className={ name }>
               { children }
             </a>
         );
     } else {
         return (
-            <GatsbyLink to={ to } className={ style.link }>
+            <GatsbyLink to={ to } className={ name }>
               { children }
             </GatsbyLink>
         )
