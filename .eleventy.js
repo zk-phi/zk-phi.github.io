ad7b9a49bac2d84200cd29af656bd14d049fdb81
zk-phi <zk-phi@users.noreply.github.com>
2021-02-17 01:31:56 +0900

    Add .eleventy.js with html minifier and image optimizer

---
+++ .eleventy.js
@@ -0,0 +1,35 @@
+const Image = require("@11ty/eleventy-img");
+const HTMLMinifier = require("html-minifier");
+
+module.exports = (eleventyConfig) => {
+    // fonts are simply copied (later optimized with subfont)
+    eleventyConfig.addPassthroughCopy("fonts");
+
+    // generate optimized images in "_site/img/" and returns an appropreate picture tag
+    eleventyConfig.addNunjucksAsyncShortcode("image", async (src, alt, sizes) => {
+        const img = await Image(src, {
+            outputDir: "_site/img/",
+            widths: [null] /* keep original */
+        });
+        return Image.generateHTML(img, {
+            alt,
+            sizes,
+            loading: "lazy",
+            decoding: "async",
+        })
+    });
+
+    // minify generated HTML (and inlined CSS/JS)
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            return HTMLMinifier.minify(content, {
+                collapseBooleanAttributes: true,
+                collapseWhitespace: true,
+                removeComments: true,
+                minifyCSS: true,
+                minifyJS: true,
+            });
+        }
+        return content;
+    });
+};
