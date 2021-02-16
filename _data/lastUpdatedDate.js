726e89b5caa1119face8185ae7a25ef04a0a3473
zk-phi <zk-phi@users.noreply.github.com>
2021-02-17 03:07:26 +0900

    Cache last-updated-date

--- _data/lastUpdatedDate.js
+++ _data/lastUpdatedDate.js
@@ -1,6 +1,14 @@
 const { Octokit } = require("@octokit/rest");
+const { AssetCache } = require("@11ty/eleventy-cache-assets");
 
 module.exports = async () => {
+    const cache = new AssetCache("lastUpdatedDate");
+
+    if (cache.isCacheValid("1d")) {
+        const value = await cache.getCachedValue();
+        return new Date(value);
+    }
+
     const octokit = new Octokit({
         auth: process.env.GITHUB_API_TOKEN
     });
@@ -10,5 +18,6 @@ module.exports = async () => {
         repo: "zk-phi.github.io",
     });
 
+    cache.save(repo.data.pushed_at);
     return new Date(repo.data.pushed_at);
 };
