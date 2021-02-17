20d3b5d7b6f9566c499d93096887bdc36b4ba3a5
zk-phi <zk-phi@users.noreply.github.com>
2021-02-17 16:09:29 +0900

    Add meishi page

---
+++ _includes/js/meishi.js
@@ -0,0 +1,6 @@
+function flip() {
+    const upside = document.getElementById("meishi-upside");
+    const downside = document.getElementById("meishi-downside");
+    upside.id = "meishi-downside";
+    downside.id = "meishi-upside";
+}
