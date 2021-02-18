b42dc4447bdaef7d1ae21f6c61b899b09fcd516a
zk-phi <zk-phi@users.noreply.github.com>
2021-02-18 14:29:30 +0900

    Otameshi: Lazy-create stalker div to reduce CLS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 !(function () {
-    // add prefetch link on mouseover
+    // --- -add prefetch link on mouseover
+
     function prefetch(e) {
         if (e.target.tagName != "A") {
             return;
@@ -20,30 +21,35 @@
         link.href = e.target.href;
         document.head.appendChild(link);
     }
+
     document.documentElement.addEventListener("mouseover", prefetch, { passive: true });
     document.documentElement.addEventListener("touchstart", prefetch, { passive: true });
 
-    // mouse-stalker (some pages do not have stalker div)
+    // ---- mouse-stalker
+
     let isTouch = false;
-    const stalker = document.getElementById("stalker");
-    if (stalker) {
-        const handleMousemove = (e) => {
-            if (!isTouch) {
-                stalker.style.transition = "opacity 0.3s";
-                stalker.style.opacity = 1;
-                stalker.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-            }
-        };
-        const handleMouseout = (e) => {
-            stalker.style.opacity = 0;
-        };
-        const handleTouchstart = (e) => {
-            isTouch = true;
-        };
-        document.addEventListener('mousemove', handleMousemove, { passive: true });
-        document.addEventListener('mouseout', handleMouseout, { passive: true });
-        document.addEventListener('touchstart', handleTouchstart, { passive: true });
-    }
+
+    const stalker = document.createElement("div");
+    stalker.id = "stalker";
+    document.body.appendChild(stalker);
+
+    const handleMousemove = (e) => {
+        if (!isTouch) {
+            stalker.style.transition = "opacity 0.3s";
+            stalker.style.opacity = 1;
+            stalker.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+        }
+    };
+    const handleMouseout = (e) => {
+        stalker.style.opacity = 0;
+    };
+    const handleTouchstart = (e) => {
+        isTouch = true;
+    };
+
+    document.addEventListener('mousemove', handleMousemove, { passive: true });
+    document.addEventListener('mouseout', handleMouseout, { passive: true });
+    document.addEventListener('touchstart', handleTouchstart, { passive: true });
 
     //// start loading webfont
     //document.body.classList.add("dotgothic");
