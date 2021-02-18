62b55ead0ca282275230a6aae7c4ea682fb60344
zk-phi <zk-phi@users.noreply.github.com>
2021-02-18 05:12:56 +0000

    deploy: b0b614077f1296e18b639676ceb6c4c5b7d365f8

---
+++ js/clap.js
@@ -0,0 +1,1 @@
+document.getElementById("JS_clap").onclick=function(e){window.alert("応援ありがとうございます!!"),e.target.disabled=!0};
---
+++ js/loader.js
@@ -0,0 +1,1 @@
+function load(){lazyScripts.forEach((function(e){const t=document.createElement("script");t.src=e,document.head.appendChild(t)})),lazyStyles.forEach((function(e){const t=document.createElement("link");t.rel="stylesheet",t.href=e,document.head.appendChild(t)}))}window.requestIdleCallback?window.requestIdleCallback(load,{timeout:2e3}):window.setTimeout(load,100);
