b0b614077f1296e18b639676ceb6c4c5b7d365f8
zk-phi <zk-phi@users.noreply.github.com>
2021-02-18 14:11:05 +0900

    Otameshi: Loading some resources with requestIdleCallback

---
+++ js/loader.js
@@ -0,0 +1,19 @@
+function load() {
+    lazyScripts.forEach(function (s) {
+        const e = document.createElement("script");
+        e.src = s;
+        document.head.appendChild(e);
+    });
+    lazyStyles.forEach(function (s) {
+        const e = document.createElement("link");
+        e.rel = "stylesheet";
+        e.href = s;
+        document.head.appendChild(e);
+    });
+}
+
+if (window.requestIdleCallback) {
+    window.requestIdleCallback(load, { timeout: 2000 });
+} else {
+    window.setTimeout(load, 100);
+}
