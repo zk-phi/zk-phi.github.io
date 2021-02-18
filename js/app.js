64e8b1b10808927d87a0fb79980324f1921bcb03
zk-phi <zk-phi@users.noreply.github.com>
2021-02-18 13:45:11 +0900

    Wrap app.js with immediate fn for better mangling

--- js/app.js
+++ js/app.js
@@ -1,48 +1,50 @@
-// add prefetch link on mouseover
-function prefetch(e) {
-    if (e.target.tagName != "A") {
-        return;
-    }
-    if (e.target["data-prefetched"]) {
-        return;
-    }
-    if (e.target.origin != location.origin) {
-        return;
-    }
-    if (window.location.href === e.target.href) {
-        return;
-    }
-    e.target["data-prefetched"] = true;
-
-    const link = document.createElement("link");
-    link.rel = "prefetch";
-    link.href = e.target.href;
-    document.head.appendChild(link);
-}
-document.documentElement.addEventListener("mouseover", prefetch, { passive: true });
-document.documentElement.addEventListener("touchstart", prefetch, { passive: true });
-
-// mouse-stalker
-let isTouch = false;
-const stalker = document.getElementById("stalker");
-if (stalker) {
-    const handleMousemove = (e) => {
-        if (!isTouch) {
-            stalker.style.transition = "opacity 0.3s";
-            stalker.style.opacity = 1;
-            stalker.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+!(function () {
+    // add prefetch link on mouseover
+    function prefetch(e) {
+        if (e.target.tagName != "A") {
+            return;
         }
-    };
-    const handleMouseout = (e) => {
-        stalker.style.opacity = 0;
-    };
-    const handleTouchstart = (e) => {
-        isTouch = true;
-    };
-    document.addEventListener('mousemove', handleMousemove, { passive: true });
-    document.addEventListener('mouseout', handleMouseout, { passive: true });
-    document.addEventListener('touchstart', handleTouchstart, { passive: true });
-}
+        if (e.target["data-prefetched"]) {
+            return;
+        }
+        if (e.target.origin != location.origin) {
+            return;
+        }
+        if (window.location.href === e.target.href) {
+            return;
+        }
+        e.target["data-prefetched"] = true;
 
-// start loading webfont
-document.body.classList.add("dotgothic");
+        const link = document.createElement("link");
+        link.rel = "prefetch";
+        link.href = e.target.href;
+        document.head.appendChild(link);
+    }
+    document.documentElement.addEventListener("mouseover", prefetch, { passive: true });
+    document.documentElement.addEventListener("touchstart", prefetch, { passive: true });
+
+    // mouse-stalker
+    let isTouch = false;
+    const stalker = document.getElementById("stalker");
+    if (stalker) {
+        const handleMousemove = (e) => {
+            if (!isTouch) {
+                stalker.style.transition = "opacity 0.3s";
+                stalker.style.opacity = 1;
+                stalker.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+            }
+        };
+        const handleMouseout = (e) => {
+            stalker.style.opacity = 0;
+        };
+        const handleTouchstart = (e) => {
+            isTouch = true;
+        };
+        document.addEventListener('mousemove', handleMousemove, { passive: true });
+        document.addEventListener('mouseout', handleMouseout, { passive: true });
+        document.addEventListener('touchstart', handleTouchstart, { passive: true });
+    }
+
+    // start loading webfont
+    document.body.classList.add("dotgothic");
+})();
