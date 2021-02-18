cc087c303c5c7938a4bc4ece91a8c95e30448d69
zk-phi <zk-phi@users.noreply.github.com>
2021-02-18 15:36:18 +0900

    Fix: Hide (again) stalker on mouseout

--- js/app.js
+++ js/app.js
@@ -38,7 +38,13 @@
                 stalker.id = "stalker";
                 document.body.appendChild(stalker);
             }
+            stalker.style.opacity = 1;
             stalker.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+        }
+    };
+    const handleMouseout = (e) => {
+        if (stalker) {
+            stalker.style.opacity = 0;
         }
     };
     const handleTouchstart = (e) => {
@@ -46,6 +52,7 @@
     };
 
     document.addEventListener('mousemove', handleMousemove, { passive: true });
+    document.addEventListener('mouseout', handleMouseout, { passive: true });
     document.addEventListener('touchstart', handleTouchstart, { passive: true });
 
     //// start loading webfont
