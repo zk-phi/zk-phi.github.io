f4292188b43c6215034bb47be5188370901cb47a
zk-phi <zk-phi@users.noreply.github.com>
2021-02-24 14:31:05 +0900

    Rename some tags

--- _data/activityTagUrls.js
+++ _data/activityTagUrls.js
@@ -1,8 +1,8 @@
 module.exports = {
     "": "",
     "OSS": "tags/oss",
-    "テック記事": "tags/tech",
-    "趣味記事": "tags/hobby",
+    "技術": "tags/tech",
+    "趣味": "tags/hobby",
     "イベント": "tags/event",
     "その他": "tags/other",
     "メモ": "tags/memo",
