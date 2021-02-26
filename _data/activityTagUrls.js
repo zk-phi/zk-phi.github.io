8156352a114f1176b52b6bf14d077446bcc94306
zk-phi <zk-phi@users.noreply.github.com>
2021-02-27 01:41:53 +0900

    Fix url

--- _data/activityTagUrls.js
+++ _data/activityTagUrls.js
@@ -1,9 +1,9 @@
 module.exports = {
     "": "",
-    "OSS": "tags/oss",
-    "記事": "tags/article",
-    "発表": "tags/talk",
-    "イベント": "tags/event",
-    "その他": "tags/other",
-    "メモ": "tags/memo",
+    "OSS": "tags/oss/",
+    "記事": "tags/article/",
+    "発表": "tags/talk/",
+    "イベント": "tags/event/",
+    "その他": "tags/other/",
+    "メモ": "tags/memo/",
 };
