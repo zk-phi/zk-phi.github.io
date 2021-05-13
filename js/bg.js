d4dd38473be75c57ae7143dfa4f24217e1916a38
zk-phi <zk-phi@users.noreply.github.com>
2021-05-13 23:50:31 +0900

    Use non-transparent color for performance

--- js/bg.js
+++ js/bg.js
@@ -12,7 +12,7 @@
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "rgb(255, 255, 255, 0.5)";
+  ctx.fillStyle = "#9884ad";
 
   const stars = [];
   for (let j = 0; j < LAYERS.length; j++) {
